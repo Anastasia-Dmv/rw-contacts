cf5ca7fe138cf3c26b4563aa91614aed36426f3c
Anastasia-Dmv <Vesta50000@gmail.com>
2020-11-10 16:33:19 +0200

    part of ...

--- src/App.js
+++ src/App.js
@@ -1,10 +1,48 @@
-import "./App.css";
+import React from "react";
 import { Contacts } from "./pages/Contacts";
 
-export function App() {
+export const App = () => {
   return (
     <div>
       <Contacts />
     </div>
   );
-}
+};
+// import React from "react";
+// import { useState, useEffect } from "react";
+
+// //import { Contacts } from "./pages/Contacts";
+
+// export function Contacts() {
+//   const [contacts, setContacts] = useState([]);
+//   const [isLoading, setIsLoading] = useState(false);
+//   const [isError, setIsError] = useState(false);
+
+//   useEffect(() => {
+//     const getContacts = async () => {
+//       try {
+//         setIsLoading(true);
+//         const response = await fetch("https://randomuser.me/api/?results=100");
+//         console.log("response", response);
+//         const { results } = await response.json();
+//         console.log("results", results[0]);
+//         setContacts(results);
+//         setIsError(false);
+//         setIsLoading(false);
+//       } catch (error) {
+//         setIsError(true);
+//         // setIsLoading(false);
+//       } finally {
+//         // setIsLoading(false);
+//       }
+//     };
+//     getContacts();
+//   }, []);
+//   // if (isLoading) {
+//   //   return <div>...Loading</div>;
+//   // }
+//   if (isError) {
+//     return <div>...error</div>;
+//   }
+//   return <div>Contacts {contacts}</div>;
+// }
--- src/pages/Contacts/index.js
+++ src/pages/Contacts/index.js
@@ -1,5 +1,47 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Grid from "@material-ui/core/Grid";
+import Container from "@material-ui/core/Container";
+//import Paper from "@material-ui/core/Paper";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles, createStyles } from "@material-ui/core/styles";
+import { ContactsTable } from "./ContactsTable";
+import { useContacts } from "./useContacts";
+
+const useStyles = makeStyles((theme) =>
+  createStyles({
+    root: {
+      marginTop: theme.spacing(4),
+    },
+    headContainer: {
+      marginBottom: theme.spacing(3),
+    },
+  })
+);
 
 export const Contacts = () => {
-  return <div>Contacts</div>;
+  const classes = useStyles();
+  const contacts = useContacts();
+
+  return (
+    <Container className={classes.root}>
+      <Grid container>
+        <Grid item xs={12} className={classes.headContainer}>
+          <Typography variant="h3" component="h1">
+            Contacts
+          </Typography>
+        </Grid>
+        <Grid item xs={12}>
+          {(() => {
+            if (contacts.isLoading) {
+              return <div>...Loading</div>;
+            }
+            if (contacts.isError) {
+              return <div>...error</div>;
+            }
+            return <ContactsTable data={contacts.data} />;
+          })()}
+        </Grid>
+      </Grid>
+    </Container>
+  );
 };
